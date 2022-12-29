9c6ae286ddf6ac14d97c584407080c013da309c1
prabha442 <93595774+prabha442@users.noreply.github.com>
2022-12-29 10:55:13 +0530

    Update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -2,6 +2,11 @@
 
 resource "aws_ecr_repository" "aws-ecr" {
   name = "${var.app_name}-${var.app_environment}-ecr"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  
   tags = {
     Name        = "${var.app_name}-ecr"
     Environment = var.app_environment
